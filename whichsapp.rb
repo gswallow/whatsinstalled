16b46bcd3fff0216687a3e9fa83f8d1bb1ea45de
Greg Swallow <gswallow@gmail.com>
2015-04-20 18:33:47 -0400

    simplify

--- whichsapp.rb
+++ whichsapp.rb
@@ -1,34 +1,42 @@
 #!/usr/bin/env ruby
 
-require 'sinatra/base'
-require 'sinatra/config_file'
-require 'etcd'
 require 'yaml'
+require 'etcd'
 
-class Whichsapp < Sinatra::Base
-  register Sinatra::ConfigFile
-  config_file File.expand_path('../config.yml', __FILE__)
+config = YAML.load(File.expand_path('../config.yml', __FILE__))
+etcd = Etcd.client(host: config['settings']['info_server'], port: 4001)
 
-  configure :production, :development do
-    enable :logging
-  end
+puts etcd.get('/apps')
 
-  before do
-    @etcd = Etcd.client(host: settings['settings']['info_server'], port: 4001)
-  end
-
-  get '/apps' do
-    @etcd.get('/apps').inspect.to_s
-    #erb :page, :locals => { :page => 'apps', :key => @etcd.get('/apps/') }
-  end
-
-  # get '/assays' do
-  #   erb :page, :locals => { :page => 'assays', :key => @etcd.get('/assays/') }
-  # end
-  #
-  # get '/packages' do
-  #   erb :page, :locals => { :page => 'packages', :key => @etcd.get('/packages/') }
-  # end
-
-  run! if app_file == $0
-end
+# require 'sinatra/base'
+# require 'sinatra/config_file'
+# require 'etcd'
+# require 'yaml'
+#
+# class Whichsapp < Sinatra::Base
+#   register Sinatra::ConfigFile
+#   config_file File.expand_path('../config.yml', __FILE__)
+#
+#   configure :production, :development do
+#     enable :logging
+#   end
+#
+#   before do
+#     @etcd = Etcd.client(host: settings['settings']['info_server'], port: 4001)
+#   end
+#
+#   get '/apps' do
+#     @etcd.get('/apps').inspect.to_s
+#     #erb :page, :locals => { :page => 'apps', :key => @etcd.get('/apps/') }
+#   end
+#
+#   # get '/assays' do
+#   #   erb :page, :locals => { :page => 'assays', :key => @etcd.get('/assays/') }
+#   # end
+#   #
+#   # get '/packages' do
+#   #   erb :page, :locals => { :page => 'packages', :key => @etcd.get('/packages/') }
+#   # end
+#
+#   run! if app_file == $0
+# end
