bffe2c063d6db7912e12c295a243a45c9d8d3ddd
Greg Swallow <gswallow@gmail.com>
2015-05-04 11:47:51 -0400

    test packages

--- whichsapp.rb
+++ whichsapp.rb
@@ -47,50 +47,10 @@ class Whichsapp
     self.value_of("#{server}/timestamp") rescue nil
   end
 
-  def get_packages
-    self.get_children('/packages')
-  end
-
-  def get_assays
-    self.get_children('/assays')
-  end
-
   def name_of(key)
     File.basename(key)
   end
 end
 
 puts Whichsapp.new.get_versions_and_timestamps('/apps')
-
-# require 'sinatra/base'
-# require 'sinatra/config_file'
-# require 'etcd'
-# require 'yaml'
-#
-# class Whichsapp < Sinatra::Base
-#   register Sinatra::ConfigFile
-#   config_file File.expand_path('../config.yml', __FILE__)
-#
-#   configure :production, :development do
-#     enable :logging
-#   end
-#
-#   before do
-#     @etcd = Etcd.client(host: settings['settings']['info_server'], port: 4001)
-#   end
-#
-#   get '/apps' do
-#     @etcd.get('/apps').inspect.to_s
-#     #erb :page, :locals => { :page => 'apps', :key => @etcd.get('/apps/') }
-#   end
-#
-#   # get '/assays' do
-#   #   erb :page, :locals => { :page => 'assays', :key => @etcd.get('/assays/') }
-#   # end
-#   #
-#   # get '/packages' do
-#   #   erb :page, :locals => { :page => 'packages', :key => @etcd.get('/packages/') }
-#   # end
-#
-#   run! if app_file == $0
-# end
+puts Whichsapp.new.get_versions('/packages')
